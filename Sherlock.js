a8fffaa7219d445381124028aacf5df2d72f0691
salim <msm2243@columbia.edu>
2017-03-01 00:12:33 -0500

    only case #4 not working

--- Sherlock.js
+++ Sherlock.js
@@ -1,3 +1,7 @@
+/*
+This is a challenge from HackerRank
+**/
+
 process.stdin.resume();
 process.stdin.setEncoding('ascii');
 
@@ -27,8 +31,26 @@ function main() {
     var q = parseInt(n_temp[2]);
     a = readLine().split(' ');
     a = a.map(Number);
+
+    var ka = new Array(k);
+
+    for(var k0 = 0; k0 < k; k0++ ) {
+        ka[k0] = a[n -k0 -1];
+    }
+
+
+    for(var n0 =n-1; n0 > k-1; n0--) {
+        a[n0]= a[n0-k];
+    }
+
+    for(k0 = 0; k0 < k; k0++) {
+        a[k0] = ka[k-k0-1]
+    }
+
+
     for(var a0 = 0; a0 < q; a0++){
         var m = parseInt(readLine());
+        console.log(a[m])
     }
 
 }
